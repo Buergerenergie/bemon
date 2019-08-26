dfdd0bd5777b97c6a661c2a97e8d01157ceaee49
Walther <thomas.walther01@sap.com>
2019-08-26 10:40:38 +0200

    solaredge support added

--- solaredge/solardge.php
+++ solaredge/solardge.php
@@ -20,31 +20,17 @@ $longitude = "13.0000000";
 $content = file_get_contents("https://monitoringapi.solaredge.com/site/".$ID."/overview?api_key=".$APIkey );
 $json=json_decode($content);
 //print_r($json);
-
-echo "Last Update Time: ".$json->overview->lastUpdateTime;
-echo PHP_EOL;
-echo "Last Year Data: ".$json->overview->lastYearData->energy;
-echo PHP_EOL;
-echo "Last Month Data: ".$json->overview->lastMonthData->energy;
-echo PHP_EOL;
-echo "Last Day Data: ".$json->overview->lastDayData->energy;
-echo PHP_EOL;
-echo "Current Power: ".$json->overview->currentPower->power;
-
 $unixtimestamp = strtotime($json->overview->lastUpdateTime);
 
 // directly get the database object
-$database = InfluxDB\Client::fromDSN(sprintf('influxdb://'.$influxuser.':'.$influxpw.'.@%s:%s/%s', $influxhost, $influxport, $influxdbname));
-// get the client to retrieve other databases
-$client = $database->getClient();
+$database = InfluxDB\Client::fromDSN(sprintf('influxdb://'.$influxuser.':'.$influxpw.'@%s:%s/%s', $influxhost, $influxport, $influxdbname));
 
 $points = [
 	new InfluxDB\Point(
 		'energy', // the name of the measurement
 		null, // measurement value
 		['dev_id' => $dev_id, 'cooperative' => $cooperative, 'dev_name' => $dev_name, 'dev_location' => $dev_location], // measurement tags
-		['harvestday' => 10, 'harvestweek' => 1, 'harvestmonth' => 10, 'harvestyear' => 10, 'longitude' => 10, 'latitude' => 10], // measurement fields
-		exec($unixtimestamp) // timestamp in nanoseconds on Linux ONLY
+		['harvestday' => (int)$json->overview->lastDayData->energy, 'harvestmonth' => (int)$json->overview->lastMonthData->energy, 'harvestyear' => (int)$json->overview->lastYearData->energy, 'power' => (int)$json->overview->currentPower->power, 'longitude' => (float)$longtitude, 'latitude' => (float)$latitude], $unixtimestamp
 	)
 ];
 $newPoints = $database->writePoints($points, InfluxDB\Database::PRECISION_SECONDS);
